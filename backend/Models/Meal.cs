91d039db3be730704ffd7de42c06948426647123
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-08-02 17:16:33 +0100

    MealDto updated

--- backend/Models/Meal.cs
+++ backend/Models/Meal.cs
@@ -6,6 +6,7 @@ namespace API.Models
     {
         public int Id { get; set; }
         public string Description { get; set; }
+        public int Calories { get; set; }
         public DateTime Added { get; set; }
         public int UserId { get; set; }
     }
